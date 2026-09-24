02140ca7eb1448d07a510b32c73e29b543cd974a
KKTLOLS <31997886+KKTLOLS@users.noreply.github.com>
2026-09-24 19:11:15 +0300

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,33 @@
+```javascript
+const CACHE_NAME = "jurnal-barber-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
+```
